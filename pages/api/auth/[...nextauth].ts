d151fda59f05a1d6e7b66b6dbb09bb3954daeb8f
bantoinese83 <b.antoine.se@gmail.com>
2025-01-25 12:14:10 -0500

    init

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,4 +1,4 @@
-import NextAuth from "next-auth"
+import NextAuth, { DefaultSession } from "next-auth"
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import { PrismaClient } from "@prisma/client"
 import GoogleProvider from "next-auth/providers/google"
@@ -6,15 +6,29 @@ import GitHubProvider from "next-auth/providers/github"
 
 const prisma = new PrismaClient()
 
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string
+      role: string
+    } & DefaultSession["user"]
+  }
+
+  interface User {
+    id: string
+    role: string
+  }
+}
+
 export default NextAuth({
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
     }),
     GitHubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      clientId: process.env.GITHUB_CLIENT_ID || "",
+      clientSecret: process.env.GITHUB_CLIENT_SECRET || "",
     }),
   ],
   adapter: PrismaAdapter(prisma),
@@ -22,9 +36,11 @@ export default NextAuth({
     strategy: "jwt",
   },
   callbacks: {
-    async session({ session, token, user }) {
-      session.user.id = user.id
-      session.user.role = user.role
+    async session({ session, user }) {
+      if (session.user) {
+        session.user.id = user.id
+        session.user.role = user.role
+      }
       return session
     },
   },
